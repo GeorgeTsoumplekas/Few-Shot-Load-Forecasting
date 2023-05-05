74b1f6cdb12dd33f7c61dcb6f987e1eaa98c1e18
GeorgeTsoumplekas <giwrikaspro@gmail.com>
2023-05-05 21:17:35 +0300

    Create clustering prototype

--- hsml/source/model_builder.py
+++ hsml/source/model_builder.py
@@ -4,12 +4,12 @@ from torch import nn
 
 class HierarchicalClustering(nn.Module):
     
-    def __init__(self, num_levels, num_clusters, sigma, embedding_size):
+    def __init__(self, num_levels, num_centers, sigma, embedding_size):
 
         super().__init__()
 
         self.num_levels = num_levels
-        self.num_clusters = num_clusters
+        self.num_centers = num_centers
         self.embedding_size = embedding_size
         self.sigma = sigma
 
@@ -24,24 +24,60 @@ class HierarchicalClustering(nn.Module):
 
         # TODO: determine how to initialize cluster centers
         for i in range(1, self.num_levels):
-            for j in range(num_clusters[i-1]):
-                center_name = "layer_" + str(i) + "_center_" + str(j+1)
+            for j in range(self.num_centers[i]):
+                center_name = "level_" + str(i) + "_center_" + str(j+1)
                 self.cluster_centers[center_name] = nn.Parameter(
-                    data=torch.ones(self.embedding_size), requires_grad=True)
+                    data=torch.rand(self.embedding_size), requires_grad=True)
                 
 
-    def assignment_step(self, h_i, layer):
-        pass
+    def assignment_step(self, h_i, level):
+
+        # level starts from 1, not 0, i.e. 1,2,3 for 4 levels
+        p_i = torch.zeros(self.num_centers[level-1], self.num_centers[level])
+
+        for i, h_i_cluster in enumerate(h_i):  # test we get a correct h_i_cluster each time
+            assignment_scores = torch.zeros(self.num_centers[level])
+
+            for center_idx in range(self.num_centers[level]):
+                center_name = "level_" + str(level) + "_center_" + str(center_idx+1)
+
+                assignment_scores[center_idx] = -torch.sum(torch.square(
+                    h_i_cluster - self.cluster_centers[center_name]) /
+                    (2.0 * self.sigma))
+
+            p_i[i][:] = torch.softmax(assignment_scores, dim=0)  # test it runs ok for all levels
+
+        return p_i
 
 
-    def update_step(self):
-        pass
-                
-    
+    def update_step(self, p_i, h_i, level):
+
+        # level starts from 1, not 0, i.e. 1,2,3 for 4 levels
+        h_i_next = torch.zeros(self.num_centers[level], self.embedding_size)
+
+        for i in range(self.num_centers[level]):
+            layer_name = "linear_" + str(level)
+
+            for j in range(self.num_centers[level-1]):
+                h_i_next[i] += (p_i[j][i] * torch.tanh(self.linear_layers[layer_name](h_i[j])))
+
+        return h_i_next
+ 
+
     def forward(self, task_embedding):
-        pass
+        
+        h_level = task_embedding
+
+        for level in range(1, self.num_levels):
+            p_level = self.assignment_step(h_level, level)
+            h_level = self.update_step(p_level, h_level, level)
+
+        return h_level
 
 
-def build_cluster_network(num_levels, num_clusters, embedding_size, device):
-    cluster_network = HierarchicalClustering(num_levels, num_clusters, embedding_size).to(device)
+def build_cluster_network(num_levels, num_centers, sigma, embedding_size, device):
+    cluster_network = HierarchicalClustering(num_levels,
+                                             num_centers,
+                                             sigma,
+                                             embedding_size).to(device)
     return cluster_network
--- hsml/source/train.py
+++ hsml/source/train.py
@@ -1,20 +1,51 @@
+import argparse
+import json
+import os
+import yaml
+
 import torch
 
 import model_builder
 
 
 def main():
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--train_dir', dest='train_dir')
+    # parser.add_argument('--test_dir', dest='test_dir')
+    # parser.add_argument('--config', '-C', dest='config_filepath')
+    # args = parser.parse_args()
+
+    # train_dir = args.train_dir
+    # test_dir = args.test_dir
+    # config_filepath = args.config_filepath
+    # with open(config_filepath, 'r', encoding='utf8') as stream:
+    #     config = yaml.safe_load(stream)
+
+    train_embeddings_filename = "../../data/mini_iONA_train_embeddings.json"
+    with open(train_embeddings_filename, 'r', encoding='utf8') as stream:
+        embeddings = json.load(stream)
+
+    task_embedding = torch.tensor(embeddings['002']).unsqueeze(dim=0)
+
     num_levels = 4
-    num_clusters = [4 ,2, 1]
-    embedding_size = 128
+    num_centers = [1, 4 ,2, 1]
+    embedding_size = 134
     device = torch.device("cpu")
+    sigma = 2.0
+
     network = model_builder.build_cluster_network(num_levels,
-                                                  num_clusters,
+                                                  num_centers,
+                                                  sigma,
                                                   embedding_size,
                                                   device)
+
+    h_i_final = network(task_embedding)
+
+    # for key, value in network.named_parameters():
+    #     print(key, value.shape)
+
     
-    for key, value in network.named_parameters():
-        print(key, value.shape)
 
 
 if __name__ == "__main__":
